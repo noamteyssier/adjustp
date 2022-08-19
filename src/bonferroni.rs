d7bb0513e274ebb4fc9d9fdc760c5d5f55b49aba
Noam Teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-08-19 14:40:26 -0700

    9 formatting (#10)
    * remove unnecessary whitespace
    
    * unroll newlines to be consistent with formatting scheme
    
    * bump version

--- src/bonferroni.rs
+++ src/bonferroni.rs
@@ -2,23 +2,23 @@ use std::ops::Mul;
 
 /// Performs the Bonferroni Correction
 pub struct Bonferroni {
-    num_elements: f64
+    num_elements: f64,
 }
 impl Bonferroni {
     /// Creates a new instance of Bonferroni
-    #[must_use] 
+    #[must_use]
     pub fn new(num_elements: f64) -> Self {
         Self { num_elements }
     }
 
     /// Calculates the adjusted pvalue given the pvalue
-    #[must_use] 
+    #[must_use]
     pub fn adjust(&self, pvalue: f64) -> f64 {
         pvalue.mul(self.num_elements).min(1.)
     }
 
     /// Performs the procedure on a slice of floats
-    #[must_use] 
+    #[must_use]
     pub fn adjust_slice(slice: &[f64]) -> Vec<f64> {
         let b = Self::new(slice.len() as f64);
         slice.iter().map(|x| b.adjust(*x)).collect()
@@ -28,7 +28,7 @@ impl Bonferroni {
 #[cfg(test)]
 mod testing {
     use super::Bonferroni;
-    
+
     #[test]
     fn example() {
         let pvalues = vec![0.1, 0.2, 0.3, 0.4];
